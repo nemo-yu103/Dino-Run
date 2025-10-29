615660b766c8ab4b92eeb50173a848cf4db22458
turuyosi <kamuko.if15@gmail.com>
2025-10-29 18:16:16 +0900

    map

---
+++ Assets/Sclipts/ScrollMap.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ScrollMap : MonoBehaviour
+{
+    public float scrollSpeed = 2f;
+    public float resetPositionX = -20f;
+    public float startPositionX = 20f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
+        if (transform.position.x <= resetPositionX)
+        {
+            transform.position = new Vector3(startPositionX,transform.position.y,transform.position.z);
+        }
+    }
+}
