6c0e4c0a4cbdfdeb3b503014e1a0493d6af43072
Patcharida07 <game22403030@gmail.com>
2026-01-08 12:30:36 +0900

    scoreUI

--- Assets/Sclipts/UI/UI_Score.cs
+++ Assets/Sclipts/UI/UI_Score.cs
@@ -31,6 +31,20 @@ public class UI_Score : MonoBehaviour
 
     void UpdateDigits()
     {
-        int one = ;
+        int one = scoreManager.gameScore % 10;
+        int ten = scoreManager.gameScore / 10;
+        int hundred = scoreManager.gameScore / 100;
+        int thousand = scoreManager.gameScore / 1000;
+        int ten_thousand= scoreManager.gameScore / 10000;
+        int one_hundred_thousand = scoreManager.gameScore / 100000;
+        int million = scoreManager.gameScore / 1000000;
+        int ten_million = scoreManager.gameScore / 10000000;
+        int one_hundred_million = scoreManager.gameScore / 100000000;
+
+        sr1.SetCategoryAndLabel("1", one.ToString());
+        sr10.SetCategoryAndLabel("10", ten.ToString());
+        sr100.SetCategoryAndLabel("100", hundred.ToString());
+        sr1000.SetCategoryAndLabel("1000", thousand.ToString());
+        sr10000.SetCategoryAndLabel("10000", ten_thousand.ToString());
     }
 }
